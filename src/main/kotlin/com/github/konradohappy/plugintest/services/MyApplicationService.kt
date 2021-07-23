89b8dbff267d37e02b7a222001550d79e3de35f7
GitHub Action <action@github.com>
2021-07-23 22:03:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/konradohappy/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/konradohappy/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.konradohappy.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.konradohappy.plugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/konradohappy/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/konradohappy/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.konradohappy.plugintest.services
 
+import com.github.konradohappy.plugintest.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
